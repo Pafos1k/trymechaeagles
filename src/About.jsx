819e5af305824cce70a02f9c1af1db718fc15320
Uladzislau Hoila <hoila@bc.edu>
2025-11-04 01:27:59 -0500

    Fix case sensitivity in About.css import

--- src/About.jsx
+++ src/About.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import DomeGallery from "./components/DomeGallery";
 import ShinyText from "./components/ShinyText";
-import "./about.css";
+import "./About.css";
 
 export default function About() {
   const [active, setActive] = useState("who");
